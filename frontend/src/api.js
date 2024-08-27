6d536fedaa880787cd9e05237102104b1e45d44d
Daniele <busond5@gmail.com>
2024-08-27 17:54:39 +0200

    minor changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,6 +7,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
+console.log("API Base URL:", import.meta.env.VITE_API_URL);
+
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
